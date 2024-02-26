e4a7faadd02b9a6123ab0284ad1502c08705fbc0
ferranoliverg <155559822+ferranoliverg@users.noreply.github.com>
2024-02-26 10:47:46 +0100

    E3
    Updated code

--- Exercise 3/L4 RTO_Example/RTO_Example/call_RTO3.m
+++ Exercise 3/L4 RTO_Example/RTO_Example/call_RTO3.m
@@ -6,8 +6,7 @@ close all
 % Saturation concentration of oxygen
 P_O2 = 0.21; % atm
 k_H = 0.027 * exp(1142 / 318.5); % mmol O2 / L * atm (calculated at max temp)
-osat = ((P_O2 * k_H) / 36); % (mmol O2 / L) / (mg/mmol)
-
+osat = ((P_O2 * k_H) / 32); % (mmol O2 / L) / (mg/mmol)
 
 %temperature dependency
 A = 1E10; %(h-1) 
@@ -34,7 +33,7 @@ par = [Ks, Ko, Yxs, V, sf, of, mumax, osat, VC, C];
 
 % Define limits for the variables if possible
 
-xMax=[1    318.5      1000      10       10         osat]; % Upper boundary for F, T, kLa, X, S, DO
+xMax=[1    318.5      1000      10       10         0.0064]; % Upper boundary for F, T, kLa, X, S, DO
 xMin=[0    288.5      0      0.1       0           0]; % Lower boundary for F, T, kLa, X, S, DO
 
 % Define guess for initial value
@@ -47,43 +46,43 @@ options=optimset('MaxFunEvals',1e5,'MaxIter ',1e4,'Display','on','TolFun',1e-8,'
 % variables are linked by the model (@nonlinearcon)
 
 disp('The Flow is : ')
-x1=xOpt(1)
+flow=xOpt(1)
 
 disp('The temperature is : ')
-x2=xOpt(2)
+temp=xOpt(2)
 
 disp('The kLa is: ')
-x3=xOpt(3)
+oxygen=xOpt(3)
+
+xinit=[flow temp oxygen];
+
+%Defining time simulation 
+t0 = 0; 
+t_interval = 0.5; 
+t_final = 168;
+
+% Saturation concentration of oxygen
+P_O2 = 0.21; % atm
+k_H = 0.027 * exp(1142 / temp); % mmol O2 / L * atm (calculated at max temp)
+osat_opt = ((P_O2 * k_H) / 32); % (mmol O2 / L) / (mg/mmol)
 
 
 
+%temperature dependency
+A = 1E10; %(h-1) 
+B = 3E90; %(h-1)
+Eg = 58; %(kJ/mol) 
+Gd = 550; %(kJ/mol)
+R = 8.314E-3; %kJ/(mol*K)
+mumax_opt = A * exp(-Eg/(R*temp))/(1+B*exp(-Gd/(R*temp)));
 
-%% Contour plot
-vP1=linspace(xMin(1),xMax(1),10);     % Vector values P1
-vX1=linspace(xMin(3),7,10);   % Vector values x1
-x0=[22 4 1 1];
-for i=1:length(vP1)
-    P1=vP1(i);       
-    for j=1:length(vX1)
-        x1=vX1(j);
-        par=[P1 x1];   
-        [xSolution,fval]=fminsearch(@modelSolve,x0,[],par); % this one uses simplex algorithm.
-        %[xSolution,fval]=fsolve(@modelSolve1,x0,[],par); % this one uses
-        %fsolve algoritm
-        x3=xSolution(3);
-        cost(j,i)=x1+x3;   %Cost for the corresponding value of P1 and P3
-    end        
-end
+par = [Ks, Ko, Yxs, V, sf, of, mumax_opt, osat_opt];
 
-% Plot contour
-[xP1, yX1] = meshgrid(vP1, vX1);
-contourf(xP1, yX1, cost, 50, 'LineWidth', 2);
-colorbar;
-title('Contour Plot of Cost as a Function of P1 and x1');
-xlabel('Power to First Generator P1');
-ylabel('Fuel to First Generator x_1');
-set(gca,'LineWidth',2,'FontSize',14);
-axis tight;
 
-% Save plot
-saveas(gcf, 'contourplot.png');
+%Definesimulation options
+%Simulation options wereset in our_simulinkmSfunc.mdl;
+exercise3_simulink;
+%Solve the model
+sim('exercise3_simulink')
+
+
